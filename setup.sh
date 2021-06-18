11efa9dc6e5ca2f1cfc0a8da59ce486e8465ad5a
oneearedrabbit <mail@oneearedrabbit.net>
2021-06-18 18:14:08 -0400

    More accurate nginx configuration for Plausible

--- setup.sh
+++ setup.sh
@@ -3,3 +3,7 @@
 # I cannot mount userland volume in docker-compose as 65534:65534,
 # doing this manually
 sudo chown -R 65534:65534 userland
+
+# Proxy Plausible through Nginx
+sudo mkdir -p /var/run/nginx-cache/jscache
+sudo chown -R nginx:nginx /var/run/nginx-cache
