63c267864994388f61e4d91466a09cadae3a5a2c
oneearedrabbit <mail@oneearedrabbit.net>
2021-06-23 00:30:46 -0400

    Fix sandbox/__main__.py

--- diggy-sandbox/sandbox/__main__.py
+++ diggy-sandbox/sandbox/__main__.py
@@ -6,19 +6,12 @@ from sandbox.nsjail import NsJail
 def parse_args() -> argparse.Namespace:
     """Parse the command-line arguments and return the populated namespace."""
     parser = argparse.ArgumentParser(
-        prog="sandbox", usage="%(prog)s filename username [nsjail_args ...]"
+        prog="sandbox", usage="%(prog)s username filename"
     )
-    parser.add_argument("filename", help="filename to evaluate")
     parser.add_argument("username", help="username")
-    parser.add_argument(
-        "nsjail_args", nargs="?", help="override configured NsJail options"
-    )
+    parser.add_argument("filename", help="filename to evaluate")
 
-    # nsjail_args is just a dummy for documentation purposes.
-    # Its actual value comes from all the unknown arguments.
-    # There doesn't seem to be a better solution with argparse.
-    args, unknown = parser.parse_known_args()
-    args.nsjail_args = unknown
+    args = parser.parse_known_args()
     return args
 
 
@@ -26,17 +19,7 @@ def main() -> None:
     """Evaluate file through NsJail."""
     args = parse_args()
 
-    # TODO: fix me
-    lang = langs.get(os.path.splitext(filename)[1])
-
-    # This language is not supported
-    if lang is None:
-        print(
-            f"I don't know how to run `{filename}' file. Please check langs.py configuration."
-        )
-        exit(0)
-
-    result = NsJail().run(username=username, filename=filename)
+    result = NsJail().run(username=args.username, filename=args.filename)
     print(result.stdout)
 
 
